ba323dc5ece8c5439e14f71b83f224942c816988
bxzr11 <53325598+bxzr11@users.noreply.github.com>
2019-10-30 16:08:44 +0800

    整合them

--- jf_IOT/src/main/java/com/jf/jf_iot/device/service/impl/DeviceTypeServiceImpl.java
+++ jf_IOT/src/main/java/com/jf/jf_iot/device/service/impl/DeviceTypeServiceImpl.java
@@ -2,13 +2,21 @@ package com.jf.jf_iot.device.service.impl;
 
 import com.jf.jf_iot.common.entity.PageResult;
 import com.jf.jf_iot.device.entity.DeviceType;
+import com.jf.jf_iot.device.mapper.DeviceTypeMapper;
 import com.jf.jf_iot.device.service.DeviceTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class DeviceTypeServiceImpl implements DeviceTypeService {
+
+    @Autowired
+    private DeviceTypeMapper deviceTypeMapper;
+
     @Override
     public List<DeviceType> findAll() {
+
         return null;
     }
 
